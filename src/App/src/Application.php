0449ac50c4f70d224841bbf4603c70834fa43cfd
Joey Smith <jsmith@webinertia.net>
2024-06-13 23:42:13 -0500

    Added deps to Application construct Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/App/src/Application.php
+++ src/App/src/Application.php
@@ -7,9 +7,9 @@ namespace App;
 final class Application
 {
     public function __construct(
-
+        private DepOne $depOne,
+        private DepTwo $depTwo
     ) {
-
     }
 
     public function run()
